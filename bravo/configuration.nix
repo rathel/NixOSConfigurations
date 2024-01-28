58051da7bcdc34174a5454c34c94c414151e9182
rathel <rathel@gmail.com>
2024-01-27 20:02:27 -0700

    added some packages

--- bravo/configuration.nix
+++ bravo/configuration.nix
@@ -106,8 +106,9 @@
 	environment.systemPackages = with pkgs; [
 #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
 			bat
+			fastfetch
 			fd
-			firefox
+			floorp
 			git
 			htop
 			kitty
