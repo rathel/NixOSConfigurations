f0d23f3bac55e11437575ef3ba0b4680ffa2629a
rathel <rathel@gmail.com>
2024-01-27 18:47:36 -0700

    added tmux

--- bravo/configuration.nix
+++ bravo/configuration.nix
@@ -107,17 +107,18 @@
 #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
 			bat
 			fd
-			protonup-ng
 			firefox
 			git
 			htop
 			kitty
 			neovim
-			picom
-			usbutils
 			parted
+			picom
+			protonup-ng
 			pulsemixer
 			ripgrep
+			tmux
+			usbutils
 			vivaldi
 			vivaldi-ffmpeg-codecs
 			wget
